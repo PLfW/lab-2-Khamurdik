4b882a868ab9be9a7454ef2a04bee53bb300e25e
Andriy Khamurda <Khamurdik@gmail.com>
2016-12-05 08:32:43 +0200

    Final.

--- CREATE TABLE.sql
+++ CREATE TABLE.sql
@@ -1,12 +1,9 @@
-CREATE TYPE account_permissions AS ENUM('admin','moderator', 'account');
-
 CREATE TABLE account
 (
 	id_user SERIAL PRIMARY KEY,
 	first_name VARCHAR(20) NOT NULL,
 	email VARCHAR(20) NOT NULL,
-	password VARCHAR(20) NOT NULL,
-	permissions account_permissions NOT NULL DEFAULT 'account'
+	password VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE post
@@ -14,7 +11,7 @@ CREATE TABLE post
 	id_post SERIAL PRIMARY KEY,
 	id_user INT,
 	post_text TEXT,
-	created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
+	created_at TIMESTAMPNOT NULL DEFAULT now()
 );
 
 CREATE TABLE comment
@@ -23,24 +20,24 @@ CREATE TABLE comment
 	id_user INT,
 	id_post INT,
 	comment_text varchar(255),
-	created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
+	created_at TIMESTAMP NOT NULL DEFAULT now()
 );
 
 
-INSERT INTO account (first_name, email, password) VALUES ('Василь','babala@gmail.com','bab001');
-INSERT INTO account (first_name, email, password) VALUES ('Кудярський','kudiar@gmail.com','kud002');
-INSERT INTO account (first_name, email, password) VALUES ('Крижанівський','marko@gmail.com','krizh003');
-INSERT INTO account (first_name, email, password) VALUES ('Колесник','kalipso@gmail.com','kol004');
-INSERT INTO account (first_name, email, password) VALUES ('Хамурда','khamurdik@gmail.com','ham005');
+INSERT INTO account (first_name, email, password) VALUES ('Vasya','babala@gmail.com','bab001');
+INSERT INTO account (first_name, email, password) VALUES ('Igor','kudiar@gmail.com','kud002');
+INSERT INTO account (first_name, email, password) VALUES ('Marko','marko@gmail.com','krizh003');
+INSERT INTO account (first_name, email, password) VALUES ('Fnadiy','kalipso@gmail.com','kol004');
+INSERT INTO account (first_name, email, password) VALUES ('Khamurdik','khamurdik@gmail.com','ham005');
 
-INSERT INTO post (id_user, post_text) VALUES (1,'Сон001');
-INSERT INTO post (id_user, post_text) VALUES (1,'Сон002');
-INSERT INTO post (id_user, post_text) VALUES (2,'Сон003');
-INSERT INTO post (id_user, post_text) VALUES (3,'Сон004');
-INSERT INTO post (id_user, post_text) VALUES (4,'Сон005');
-INSERT INTO post (id_user, post_text) VALUES (5,'Сон006');
-INSERT INTO post (id_user, post_text) VALUES (2,'Сон007');
+INSERT INTO post (id_user, post_text) VALUES (1,'Dream001');
+INSERT INTO post (id_user, post_text) VALUES (1,'Dream002');
+INSERT INTO post (id_user, post_text) VALUES (2,'Dream003');
+INSERT INTO post (id_user, post_text) VALUES (3,'Dream004');
+INSERT INTO post (id_user, post_text) VALUES (4,'Dream005');
+INSERT INTO post (id_user, post_text) VALUES (5,'Dream006');
+INSERT INTO post (id_user, post_text) VALUES (2,'Dream007');
 
 
-INSERT INTO comment (id_user, id_post, comment_text) VALUES (2,1,'хаха');
-INSERT INTO comment (id_user, id_post, comment_text) VALUES (3,2,'Непоганий сон.');
+INSERT INTO comment (id_user, id_post, comment_text) VALUES (2,1,'haha');
+INSERT INTO comment (id_user, id_post, comment_text) VALUES (3,2,'Not a bad dream.');
